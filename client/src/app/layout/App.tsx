5a22ec4ea67757f9f234f5dea8fb35c54eaf410a
Jardel Sousa <jardeelsousa@hotmail.com>
2022-01-09 05:17:27 -0300

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,15 @@
 import { useState } from 'react';
-import Catalog from '../../features/catalog/Catalog'
-import Header from './Header';
+import { Route } from 'react-router-dom';
 import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
+import Catalog from '../../features/catalog/Catalog'
+import HomePage from '../../features/home/HomePage';
+import ProductDetails from '../../features/catalog/ProductDetails';
+import AboutPage from '../../features/about/AboutPage';
+import ContactPage from '../../features/contact/ContactPage';
+
+
+import Header from './Header';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -25,7 +33,11 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-        <Catalog />
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/catalog" component={Catalog}/>
+        <Route path="/catalog/:id" component={ProductDetails}/>
+        <Route path="/about" component={AboutPage}/>
+        <Route path="/contact" component={ContactPage}/>
       </Container>
     </ThemeProvider>
   );
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -8,6 +8,7 @@ import {
   Button, 
   CardHeader
 } from "@mui/material";
+import { Link } from "react-router-dom";
 
 import { Product } from '../../app/models/product';
 
@@ -44,7 +45,11 @@ export default function ProductCard({ product }: Props){
       </CardContent>
       <CardActions>
         <Button size="small">Add to cart</Button>
-        <Button size="small">View</Button>
+        <Button 
+          size="small"
+          component={Link}
+          to={`/catalog/${product.id}`}
+        >View</Button>
       </CardActions>
     </Card>
   )
