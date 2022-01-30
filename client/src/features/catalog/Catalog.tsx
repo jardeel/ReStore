513d2cc393dfd5a06bce35e530abc815b8ed0d36
Jardel Sousa <jardeelsousa@hotmail.com>
2022-01-29 23:39:06 -0300

    Adding filtering to the client

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,23 +1,83 @@
 import { useEffect } from 'react';
 import  ProductList  from './ProductList';
 import LoadingComponent from '../../app/layout/LoadingComponent';
-import { fetchProductsAsync, productSelectors } from './catalogSlice';
+import { fetchFilters, fetchProductsAsync, productSelectors, setProductParams } from './catalogSlice';
+import ProductSearch from './ProductSearch';
 import { useAppDispatch, useAppSelector } from '../../app/store/configureStore';
+import { Grid, Pagination, Paper, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import RadioButtonGroup from '../../app/components/RadioButtonGroup';
+import CheckBoxButtons from '../../app/components/CheckBoxButtons';
+
+
+const sortOptions = [
+  {value: 'name', label: 'Alphabetical'},
+  {value: 'priceDesc', label: 'Price - High to low'},
+  {value: 'price', label: 'Price - Low to high'}
+]
 
 export default function Catalog(){
   const products = useAppSelector(productSelectors.selectAll);
-  const { productsLoaded, status } = useAppSelector(state => state.catalog);
+  const { productsLoaded, status,filtersLoaded, brands, types, productParams } = useAppSelector(state => state.catalog);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if(!productsLoaded) dispatch(fetchProductsAsync());
   },[productsLoaded, dispatch]);
 
+  useEffect(() => {
+    if(!filtersLoaded) dispatch(fetchFilters());
+  },[filtersLoaded, dispatch]);
+
   if(status.includes('pending')) return <LoadingComponent message="Loading products..."/>
 
   return(
-    <>
-      <ProductList products={products}/>
-    </>
+    <Grid container spacing={4}>
+      <Grid item xs={3}>
+        <Paper sx={{mb: 2}}>
+          <ProductSearch />
+        </Paper>
+        <Paper sx={{mb: 2, p: 2}}>
+          <RadioButtonGroup
+            selectedValue={productParams.orderBy}
+            options={sortOptions}
+            onChange={(e) => dispatch(setProductParams({orderBy: e.target.value}))}
+          />
+        </Paper>
+
+        <Paper sx={{mb: 2, p: 2}}>
+          <CheckBoxButtons 
+            items={brands}
+            checked={productParams.brands}
+            onChange={(items: string[]) => dispatch(setProductParams({brands: items}))}
+          />
+        </Paper>
+
+        <Paper sx={{mb: 2, p: 2}}>
+         <CheckBoxButtons 
+            items={types}
+            checked={productParams.types}
+            onChange={(items: string[]) => dispatch(setProductParams({types: items}))}
+          />
+        </Paper>
+      </Grid>
+      <Grid item xs={9}>
+        <ProductList products={products}/>
+      </Grid>
+      <Grid item xs={3} />
+      <Grid item xs={9}>
+        <Box display='flex' justifyContent='space-between' alignItems='center'>
+          <Typography>
+            Displaying 1-6 of 20 items
+          </Typography>
+          <Pagination
+            color='secondary'
+            size='large'
+            count={10}
+            page={2}
+          />
+        </Box>
+      </Grid>
+    </Grid>
   )
 }
