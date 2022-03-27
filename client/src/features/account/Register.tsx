7e92d07af78f4f4658035bed84e662ed55aa56e8
Jardel Sousa <jardeelsousa@hotmail.com>
2022-03-27 16:31:20 -0300

    Add the registration validators

--- client/src/features/account/Register.tsx
+++ client/src/features/account/Register.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import TextField from '@mui/material/TextField';
 import { Link, useHistory } from 'react-router-dom';
@@ -8,20 +7,34 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useForm } from 'react-hook-form';
 import { LoadingButton } from '@mui/lab';
-import { Container, Typography, Paper, Alert, List, AlertTitle, ListItem, ListItemText } from "@mui/material";
+import { Container, Typography, Paper } from "@mui/material";
 
 import agent from '../../app/api/agent';
+import { toast } from 'react-toastify';
 
 const theme = createTheme();
 
 export default function Register() {
   const history = useHistory();
-  const [validationErrors, setValidationErrors] = useState([]);
 
-  const { register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
+  const { register, handleSubmit, setError, formState: {isSubmitting, errors, isValid}} = useForm({
     mode: 'all'
   });
   
+  function handleApiErrors(errors: any) {
+    if(errors){
+      errors.forEach((error: string) => {
+        if(error.includes('Password')){
+          setError('password', {message: error})
+        } else if (error.includes('Email')){
+          setError('email', {message: error})
+        } else if (error.includes('Username')){
+          setError('username', {message: error})
+        }
+      })
+    }
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Container 
@@ -43,7 +56,13 @@ export default function Register() {
         <Box 
           component="form" 
           onSubmit={handleSubmit((data) =>
-            agent.Account.register(data).catch(error => setValidationErrors(error)))} 
+            agent.Account.register(data)
+              .then(() => {
+                toast.success('Registration successful - you can now login');
+                history.push('/login');
+              })
+              .catch(error => handleApiErrors(error)))
+          } 
           noValidate sx={{ mt: 1 }}
         >
           <TextField
@@ -59,7 +78,13 @@ export default function Register() {
             margin="normal"
             fullWidth
             label="Email address"
-            {...register('email', {required: 'Email is required'})}
+            {...register('email', {
+              required: 'Email is required',
+              pattern: {
+                value: /^\w+[\w-.]*@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/,
+                message: 'Not a valid email address'
+              }
+            })}
             error={!!errors.email}
             helperText={errors?.email?.message}
           />
@@ -69,22 +94,16 @@ export default function Register() {
             label="Password"
             type="password"
             autoComplete="current-password"
-            {...register('password', {required: 'Password is required'})}
+            {...register('password', {
+              required: 'Password is required',
+              pattern: {
+                value: /(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$/,
+                message: 'password does not meet complexity requirements'
+              }
+            })}
             error={!!errors.password}
             helperText={errors?.password?.message}
           />
-          {validationErrors.length > 0 && 
-            <Alert severity="error">
-              <AlertTitle>Validation Errors</AlertTitle>
-              <List>
-                {validationErrors.map(error => (
-                  <ListItem key={error}>
-                    <ListItemText>{error}</ListItemText>
-                  </ListItem>
-                ))} 
-              </List>
-            </Alert>
-          }
           <LoadingButton
             loading={isSubmitting}
             disabled={!isValid}
